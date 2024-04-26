916d932ec13a1736e06de64da4dedd36283b0e11
Sisodiya Aakash <aakash@developar.in>
2024-04-26 15:59:25 +0530

    Created Hero structure with markup and styling added parallax effect for hero instead of slider as mentioned in the description because there are several slider sections in the page

--- assets/script/main.js
+++ assets/script/main.js
@@ -22,7 +22,28 @@ function scriptWrapper($) {
        * Scroll Functions 
        */
       scrollFunctions: function () {
-         // Scroll Functions
+         // Header variation on scroll
+         // Adding colored Header class after scroll down
+         $(window).scroll(function () {
+            if ($(this).scrollTop() > 60) {
+               $('header#header').addClass('colored-header');
+            } else {
+               $('header#header').removeClass('colored-header');
+            }
+         });
+
+         // Parallax Effect for hero background image
+         const parallaxEffectHero = function () {
+            const backgroundImage = $('section.hero-section-wrap .hero-section');
+
+            $(window).on('scroll', function () {
+               const scrollPos = $(window).scrollTop();
+               backgroundImage.css('background-size', 'calc(100% + ' + scrollPos * 0.8 + 'px)');
+            });
+         };
+
+         // Execute Parallax
+         parallaxEffectHero();
       },
 
       /**
@@ -36,7 +57,22 @@ function scriptWrapper($) {
        * Registers event handlers
        */
       registerEventHandlers: function () {
-         // Event handlers
+         $(document).on('click', 'body header.header nav.navbar .brand-area a.toggle-btn', dgTheme.toggleMobileMenu);
+      },
+
+      /**
+       * On click of the toggle button of mobile header it will execute toggleMobileMenu function
+       * This function will toggle different class for mobile menu
+       */
+      toggleMobileMenu: function (event) {
+         event.preventDefault();
+         // Toggle open-mobile-menu class on body header.header .mobile-menu
+         $('body').toggleClass('oveflow-hidden');
+         $('body header.header').toggleClass('open-mobile-header');
+         $('body header.header .mobile-menu').toggleClass('open-mobile-menu');
+
+         // Toggle hide class on body header.header nav.navbar .brand-area a.toggle-btn span
+         $('body header.header nav.navbar .brand-area a.toggle-btn span').toggleClass('hide');
       },
    }; // end dgTheme
 
